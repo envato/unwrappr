1dd40204bbc30dc6deb1147d780cbcc2ddeb88d0
Joe Sustaric <joe.sustaric@envato.com>
2018-03-27 12:23:08 +1100

    adding in time

--- spike/git_pr.rb
+++ spike/git_pr.rb
@@ -3,9 +3,9 @@ require 'octokit'
 client = Octokit::Client.new(access_token: ENV['GIT_TOKEN'])
 
 puts client.user.login
-
+time = Time.now.strftime("%Y%d%m-%H%m")
 # Assumption this will be executed in the same directory as the cloned repository.
-new_branch = `git checkout -b automated_bundle_update`
+new_branch = `git checkout -b automated_bundle_update_#{time}`
 
 # Bundle Update happens here
 bundle_update = `bundle update`
@@ -13,7 +13,7 @@ bundle_update = `bundle update`
 # git add and commit
 commit_and_add = `git add -A && git commit -m "automated bundle update"`
 
-push_branch = `git push origin automated_bundle_update`
+push_branch = `git push origin automated_bundle_update_#{time}`
 
 # Submit a PR
 client.create_pull_request(
