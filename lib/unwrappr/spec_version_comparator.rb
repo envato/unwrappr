2697d9f21093cf44519408a40771ed57df24646e
Vladimir Chervanev <vchervanev.dev@gmail.com>
2018-03-27 16:05:56 +1100

    refactored SpecVersionComparator to use new input format

--- lib/unwrappr/spec_version_comparator.rb
+++ lib/unwrappr/spec_version_comparator.rb
@@ -1,29 +1,14 @@
 module Unwrappr
   class SpecVersionComparator
-    # specs are arrays of specs like 'name (version)'
-    def self.perform(specs_before, specs_after)
-      index_before = build_spec_index(specs_before)
-      index_after = build_spec_index(specs_after)
-
-      (index_before.keys + index_after.keys).uniq.sort.map do |key|
+    # specs_versions is a hash like { name: 'version' }
+    def self.perform(specs_versions_before, specs_versions_after)
+      (specs_versions_before.keys + specs_versions_after.keys).uniq.sort.map do |key|
         {
           dependency: key,
-          before: index_before[key],
-          after: index_after[key],
+          before: specs_versions_before[key],
+          after: specs_versions_after[key],
         }
       end
     end
-
-    def self.build_spec_index(specs)
-      Hash[specs.map { |item| parse_spec(item) }]
-    end
-
-    def self.parse_spec(spec)
-      md = spec.match /^([^\s]+)\s\(([^\s]+)\)$/
-      raise ArgumentError, spec if md.nil?
-      [md[1], md[2]]
-    end
-
-    private_class_method :build_spec_index, :parse_spec
   end
 end
