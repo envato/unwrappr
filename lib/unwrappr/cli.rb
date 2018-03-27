27b17311b5aa374ea42cf765318a3d72a08534e2
Joe Sustaric <joe.sustaric@envato.com>
2018-03-27 12:47:49 +1100

    adding in fakefs gem

--- lib/unwrappr/cli.rb
+++ lib/unwrappr/cli.rb
@@ -11,6 +11,17 @@ module Unwrappr
 
     def execute
       puts "Doing the unwrappr thing.."
+      # check if current directory is a git repo
+      # make branch
+      # run bundle update
+      # commit and push
+      # submit pr
+      # 'For each updated gem' do
+      #    find git log diffs
+      #    get github diff link
+      #    figure out where to annotate the pr
+      #    annotate the PR
+      # end
     end
   end
 end
