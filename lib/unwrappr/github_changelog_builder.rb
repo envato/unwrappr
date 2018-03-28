b8b71b1bd0783ea2f42a5153ead4e8941d4129e0
Pete Johns <pete.johns@envato.com>
2018-03-28 20:21:46 +1100

    Rubocop

--- lib/unwrappr/github_changelog_builder.rb
+++ lib/unwrappr/github_changelog_builder.rb
@@ -1,14 +1,12 @@
 module Unwrappr
   class GithubChangelogBuilder
-
     def self.build(repository:, base:, head:)
       Octokit.client.compare(repository, base, head)
-        .commits
-        .map(&:commit)
-        .map(&:message)
-
+             .commits
+             .map(&:commit)
+             .map(&:message)
     rescue Octokit::NotFound
-      return []
+      []
     end
   end
 end
--- spec/lib/unwrappr/github_changelog_builder_spec.rb
+++ spec/lib/unwrappr/github_changelog_builder_spec.rb
@@ -5,30 +5,30 @@ module Unwrappr
     let(:base) { double('base') }
     let(:head) { double('head') }
     # let(:response) { [double('response')] }
-    let(:commit) { double('commit', message: "hi!") }
-    let(:commit_info) { double("commit information", commit: commit)}
+    let(:commit) { double('commit', message: 'hi!') }
+    let(:commit_info) { double('commit information', commit: commit) }
     let(:response) { double('response', commits: [commit_info]) }
 
-    context "with a successful request" do
+    context 'with a successful request' do
       before do
         allow(Octokit.client).to receive(:compare).and_return(response)
       end
 
-      it "returns an array of commit messages" do
-        expect(subject).to eq(["hi!"])
+      it 'returns an array of commit messages' do
+        expect(subject).to eq(['hi!'])
       end
     end
 
-    context "with NotFound error" do
+    context 'with NotFound error' do
       before do
         allow(Octokit.client).to receive(:compare).and_raise(Octokit::NotFound)
       end
 
-      it "does not raise" do
-        expect{subject}.not_to raise_error
+      it 'does not raise' do
+        expect { subject }.not_to raise_error
       end
 
-      it "returns an empty array" do
+      it 'returns an empty array' do
         expect(subject).to eq([])
       end
     end
