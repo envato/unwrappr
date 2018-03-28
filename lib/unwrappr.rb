b6e523d4d5e3a192ee82776b583c2b5c92a123cb
Em Esc <em@curlycoder.me>
2018-03-28 16:25:05 +1100

    Fix double entry and keep alphabetical

--- lib/unwrappr.rb
+++ lib/unwrappr.rb
@@ -3,10 +3,9 @@ require 'unwrappr/bundler_command_runner'
 require 'unwrappr/cli'
 require 'unwrappr/git_command_runner'
 require 'unwrappr/github_changelog_builder'
-require 'unwrappr/octokit'
 require 'unwrappr/lock_file_comparator'
-require 'unwrappr/ruby_gems'
 require 'unwrappr/octokit'
+require 'unwrappr/ruby_gems'
 require 'unwrappr/spec_version_comparator'
 
 module Unwrappr
