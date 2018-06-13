afeb194f4d1d9514ea3eb710182f23ab14181003
Orien Madgwick <_@orien.io>
2018-06-14 09:11:16 +1000

    Add context to LockFileAnnotator spec
    Describe the test setup.

--- spec/lib/unwrappr/lock_file_annotator_spec.rb
+++ spec/lib/unwrappr/lock_file_annotator_spec.rb
@@ -14,110 +14,113 @@ module Unwrappr
     describe '#annotate' do
       subject(:annotate) { annotator.annotate }
 
-      let(:lock_file_diff_source) { instance_double(Github::PrSource) }
-      let(:annotation_sink) { instance_spy(Github::PrSink) }
-      let(:base_lock_file) { <<~BASE_FILE }
-        GEM
-          remote: https://rubygems.org/
-          specs:
-            diff-lcs (1.3)
-            rspec (3.7.0)
-              rspec-core (~> 3.7.0)
-              rspec-expectations (~> 3.7.0)
-              rspec-mocks (~> 3.7.0)
-            rspec-core (3.7.1)
-              rspec-support (~> 3.7.0)
-            rspec-expectations (3.7.0)
-              diff-lcs (>= 1.2.0, < 2.0)
-              rspec-support (~> 3.7.0)
-            rspec-mocks (3.7.0)
-              diff-lcs (>= 1.2.0, < 2.0)
-              rspec-support (~> 3.7.0)
-            rspec-support (3.7.0)
+      context 'given a Gemfile.lock that changes: '\
+              'rspec-support 3.7.0 -> 3.7.1' do
+        let(:lock_file_diff_source) { instance_double(Github::PrSource) }
+        let(:annotation_sink) { instance_spy(Github::PrSink) }
+        let(:base_lock_file) { <<~BASE_FILE }
+          GEM
+            remote: https://rubygems.org/
+            specs:
+              diff-lcs (1.3)
+              rspec (3.7.0)
+                rspec-core (~> 3.7.0)
+                rspec-expectations (~> 3.7.0)
+                rspec-mocks (~> 3.7.0)
+              rspec-core (3.7.1)
+                rspec-support (~> 3.7.0)
+              rspec-expectations (3.7.0)
+                diff-lcs (>= 1.2.0, < 2.0)
+                rspec-support (~> 3.7.0)
+              rspec-mocks (3.7.0)
+                diff-lcs (>= 1.2.0, < 2.0)
+                rspec-support (~> 3.7.0)
+              rspec-support (3.7.0)
 
-        PLATFORMS
-          ruby
+          PLATFORMS
+            ruby
 
-        DEPENDENCIES
-          rspec
+          DEPENDENCIES
+            rspec
 
-        BUNDLED WITH
-          1.16.2
-      BASE_FILE
+          BUNDLED WITH
+            1.16.2
+        BASE_FILE
 
-      let(:head_lock_file) { <<~HEAD_FILE }
-        GEM
-          remote: https://rubygems.org/
-          specs:
-            diff-lcs (1.3)
-            rspec (3.7.0)
-              rspec-core (~> 3.7.0)
-              rspec-expectations (~> 3.7.0)
-              rspec-mocks (~> 3.7.0)
-            rspec-core (3.7.1)
-              rspec-support (~> 3.7.0)
-            rspec-expectations (3.7.0)
-              diff-lcs (>= 1.2.0, < 2.0)
-              rspec-support (~> 3.7.0)
-            rspec-mocks (3.7.0)
-              diff-lcs (>= 1.2.0, < 2.0)
-              rspec-support (~> 3.7.0)
-            rspec-support (3.7.1)
+        let(:head_lock_file) { <<~HEAD_FILE }
+          GEM
+            remote: https://rubygems.org/
+            specs:
+              diff-lcs (1.3)
+              rspec (3.7.0)
+                rspec-core (~> 3.7.0)
+                rspec-expectations (~> 3.7.0)
+                rspec-mocks (~> 3.7.0)
+              rspec-core (3.7.1)
+                rspec-support (~> 3.7.0)
+              rspec-expectations (3.7.0)
+                diff-lcs (>= 1.2.0, < 2.0)
+                rspec-support (~> 3.7.0)
+              rspec-mocks (3.7.0)
+                diff-lcs (>= 1.2.0, < 2.0)
+                rspec-support (~> 3.7.0)
+              rspec-support (3.7.1)
 
-        PLATFORMS
-          ruby
+          PLATFORMS
+            ruby
 
-        DEPENDENCIES
-          rspec
+          DEPENDENCIES
+            rspec
 
-        BUNDLED WITH
-          1.16.2
-      HEAD_FILE
+          BUNDLED WITH
+            1.16.2
+        HEAD_FILE
 
-      let(:patch) { <<~PATCH }
-        @@ -14,7 +14,7 @@ GEM
-             rspec-mocks (3.7.0)
-               diff-lcs (>= 1.2.0, < 2.0)
-               rspec-support (~> 3.7.0)
-        -    rspec-support (3.7.0)
-        +    rspec-support (3.7.1)
+        let(:patch) { <<~PATCH }
+          @@ -14,7 +14,7 @@ GEM
+              rspec-mocks (3.7.0)
+                diff-lcs (>= 1.2.0, < 2.0)
+                rspec-support (~> 3.7.0)
+          -    rspec-support (3.7.0)
+          +    rspec-support (3.7.1)
 
-         PLATFORMS
-           ruby
-      PATCH
+          PLATFORMS
+            ruby
+        PATCH
 
-      before do
-        allow(::Unwrappr::RubyGems).to receive(:gem_info)
-          .with('rspec-support')
-          .and_return(spy(homepage_uri:     'home-uri',
-                          source_code_uri:  'source-uri',
-                          changelog_uri:    'changelog-uri'))
-        allow(lock_file_diff_source).to receive(:each_file)
-          .and_yield(LockFileDiff.new(filename:   'Gemfile.lock',
-                                      base_file:  base_lock_file,
-                                      head_file:  head_lock_file,
-                                      patch:      patch,
-                                      sha:        '89ee3f7d'))
-      end
+        before do
+          allow(::Unwrappr::RubyGems).to receive(:gem_info)
+            .with('rspec-support')
+            .and_return(spy(homepage_uri:     'home-uri',
+                            source_code_uri:  'source-uri',
+                            changelog_uri:    'changelog-uri'))
+          allow(lock_file_diff_source).to receive(:each_file)
+            .and_yield(LockFileDiff.new(filename:   'Gemfile.lock',
+                                        base_file:  base_lock_file,
+                                        head_file:  head_lock_file,
+                                        patch:      patch,
+                                        sha:        '89ee3f7d'))
+        end
 
-      it 'annotates gem changes' do
-        annotate
-        expect(annotation_sink).to have_received(:annotate_change)
-          .with(
-            having_attributes(name:         'rspec-support',
-                              base_version: GemVersion.new('3.7.0'),
-                              head_version: GemVersion.new('3.7.1'),
-                              filename:     'Gemfile.lock',
-                              sha:          '89ee3f7d',
-                              line_number:  5),
-            <<~MESSAGE
-              ### [rspec-support](home-uri)
+        it 'annotates gem changes' do
+          annotate
+          expect(annotation_sink).to have_received(:annotate_change)
+            .with(
+              having_attributes(name:         'rspec-support',
+                                base_version: GemVersion.new('3.7.0'),
+                                head_version: GemVersion.new('3.7.1'),
+                                filename:     'Gemfile.lock',
+                                sha:          '89ee3f7d',
+                                line_number:  5),
+              <<~MESSAGE
+                ### [rspec-support](home-uri)
 
-              **Patch** version upgrade :chart_with_upwards_trend::small_blue_diamond: 3.7.0 → 3.7.1
+                **Patch** version upgrade :chart_with_upwards_trend::small_blue_diamond: 3.7.0 → 3.7.1
 
-              [_[change-log](changelog-uri), [source-code](source-uri)_]
-            MESSAGE
-          )
+                [_[change-log](changelog-uri), [source-code](source-uri)_]
+              MESSAGE
+            )
+        end
       end
     end
   end
