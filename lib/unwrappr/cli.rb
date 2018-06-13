39574cd5939320bd98667383c89287e67c98d926
Orien Madgwick <_@orien.io>
2018-06-13 15:43:16 +1000

    Remove annotate process comments

--- lib/unwrappr/cli.rb
+++ lib/unwrappr/cli.rb
@@ -20,13 +20,6 @@ module Unwrappr
         BundlerCommandRunner.bundle_update!
         GitCommandRunner.commit_and_push_changes!
         GitCommandRunner.make_pull_request!
-        # submit pr
-        # 'For each updated gem' do
-        #    find git log diffs
-        #    get github diff link
-        #    figure out where to annotate the pr
-        #    annotate the PR
-        # end
       end
     end
   end
