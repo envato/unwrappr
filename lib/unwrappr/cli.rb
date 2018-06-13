c443a7736f029e99dc1d2f380148b52a121e630b
Orien Madgwick <_@orien.io>
2018-06-13 15:42:10 +1000

    Introduce subcommands

--- lib/unwrappr/cli.rb
+++ lib/unwrappr/cli.rb
@@ -5,26 +5,29 @@ require 'clamp'
 module Unwrappr
   # Entry point for the app
   class CLI < Clamp::Command
+    self.default_subcommand = 'all'
+
     option ['--version', '-v'], :flag, 'Show version' do
       puts "unwrappr v#{Unwrappr::VERSION}"
       exit(0)
     end
 
-    option ['-o', '--open'], 'OPEN', 'See whats changed'
-
-    def execute
-      puts 'Doing the unwrappr thing...'
-      GitCommandRunner.create_branch!
-      BundlerCommandRunner.bundle_update!
-      GitCommandRunner.commit_and_push_changes!
-      GitCommandRunner.make_pull_request!
-      # submit pr
-      # 'For each updated gem' do
-      #    find git log diffs
-      #    get github diff link
-      #    figure out where to annotate the pr
-      #    annotate the PR
-      # end
+    subcommand 'all', 'run bundle update, push to github, '\
+                      'create a pr and annotate changes' do
+      def execute
+        puts 'Doing the unwrappr thing...'
+        GitCommandRunner.create_branch!
+        BundlerCommandRunner.bundle_update!
+        GitCommandRunner.commit_and_push_changes!
+        GitCommandRunner.make_pull_request!
+        # submit pr
+        # 'For each updated gem' do
+        #    find git log diffs
+        #    get github diff link
+        #    figure out where to annotate the pr
+        #    annotate the PR
+        # end
+      end
     end
   end
 end
