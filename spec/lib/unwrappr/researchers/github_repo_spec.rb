128734b917e116376452c492170d0672ee6333be
Orien Madgwick <_@orien.io>
2018-11-03 14:57:54 +1100

    If the source_code_uri isn't set use the homepage_uri

--- spec/lib/unwrappr/researchers/github_repo_spec.rb
+++ spec/lib/unwrappr/researchers/github_repo_spec.rb
@@ -10,7 +10,12 @@ module Unwrappr
 
         let(:gem_change) { instance_double(GemChange) }
         let(:gem_change_info) { { ruby_gems: info } }
-        let(:info) { spy(:ruby_gem_info, source_code_uri: source_code_uri) }
+        let(:info) do
+          spy(:ruby_gem_info,
+              source_code_uri: source_code_uri,
+              homepage_uri: homepage_uri)
+        end
+        let(:homepage_uri) { nil }
 
         context 'given a non Github source code URI' do
           let(:source_code_uri) { 'https://bitbucket.org/envato/unwrappr/tree' }
@@ -21,6 +26,30 @@ module Unwrappr
 
           it 'returns the data provided in gem_change_info' do
             expect(research).to include(gem_change_info)
+          end
+        end
+
+        context 'given a nil source code URI' do
+          let(:source_code_uri) { nil }
+
+          it "doesn't add the Github repo" do
+            expect(research).to_not include(:github_repo)
+          end
+
+          it 'returns the data provided in gem_change_info' do
+            expect(research).to include(gem_change_info)
+          end
+
+          context 'given a Github homepage uri' do
+            let(:homepage_uri) { 'https://github.com/envato/stack_master/tree' }
+
+            it 'extracts the repo' do
+              expect(research).to include(github_repo: 'envato/stack_master')
+            end
+
+            it 'returns the data provided in gem_change_info' do
+              expect(research).to include(gem_change_info)
+            end
           end
         end
 
