c1b0fbc1a577691db6c4c766ace94e45a57955ec
Pete Johns <pete.johns@envato.com>
2020-04-13 19:07:55 +1000

    Handle anchors in homepage URI
    Playing with https://github.com/new reveealed repository names are formed of alphanumeric characters, hyphens, underscores and full-stops.
    
    https://github.com/rubymem/bundler-leak/blob/master/gemspec.yml#L7 caused unwrappr to terminate abruptly

--- spec/lib/unwrappr/researchers/github_repo_spec.rb
+++ spec/lib/unwrappr/researchers/github_repo_spec.rb
@@ -17,7 +17,8 @@ module Unwrappr
           ['', 'https://github.com/envato/stack_master/tree', 'envato/stack_master'],
           [' ', 'https://github.com/envato/stack_master/tree', 'envato/stack_master'],
           ['https://bitbucket.org/envato/unwrappr/tree', 'https://github.com/envato/stack_master/tree', 'envato/stack_master'],
-          ['https://github.com/envato/unwrappr/tree', 'https://github.com/envato/stack_master/tree', 'envato/unwrappr']
+          ['https://github.com/envato/unwrappr/tree', 'https://github.com/envato/stack_master/tree', 'envato/unwrappr'],
+          [nil, 'https://github.com/rubymem/bundler-leak#readme', 'rubymem/bundler-leak'],
         ].each do |source_code_uri, homepage_uri, expected_repo|
           context "given source_code_uri: #{source_code_uri.inspect}, homepage_uri: #{homepage_uri.inspect}" do
             let(:gem_change_info) do
