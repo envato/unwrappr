200b8ba551471a111114d15f9830ef370a7b09d8
Pete Johns <pete.johns@envato.com>
2020-04-13 19:39:37 +1000

    I disagree with Style/TrailingCommaInArrayLiteral, but that is for another time

--- spec/lib/unwrappr/researchers/github_repo_spec.rb
+++ spec/lib/unwrappr/researchers/github_repo_spec.rb
@@ -18,7 +18,7 @@ module Unwrappr
           [' ', 'https://github.com/envato/stack_master/tree', 'envato/stack_master'],
           ['https://bitbucket.org/envato/unwrappr/tree', 'https://github.com/envato/stack_master/tree', 'envato/stack_master'],
           ['https://github.com/envato/unwrappr/tree', 'https://github.com/envato/stack_master/tree', 'envato/unwrappr'],
-          [nil, 'https://github.com/rubymem/bundler-leak#readme', 'rubymem/bundler-leak'],
+          [nil, 'https://github.com/rubymem/bundler-leak#readme', 'rubymem/bundler-leak']
         ].each do |source_code_uri, homepage_uri, expected_repo|
           context "given source_code_uri: #{source_code_uri.inspect}, homepage_uri: #{homepage_uri.inspect}" do
             let(:gem_change_info) do
