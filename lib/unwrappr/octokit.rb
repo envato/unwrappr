02538f38bcb9c0c416cd1f477d7c9ec1b35c710e
Orien Madgwick <_@orien.io>
2022-08-06 10:07:32 +1000

    Rubocop: resolve Style/FetchEnvVar

--- lib/unwrappr/octokit.rb
+++ lib/unwrappr/octokit.rb
@@ -3,6 +3,19 @@
 # Wrapper around octokit
 module Octokit
   def self.client
-    @client ||= Client.new(access_token: ENV['GITHUB_TOKEN'])
+    @client ||= Client.new(access_token: access_token_from_environment)
+  end
+
+  def self.access_token_from_environment
+    ENV.fetch('GITHUB_TOKEN') do
+      raise <<~MESSAGE
+        Missing environment variable GITHUB_TOKEN.
+        See https://github.com/settings/tokens to set up personal access tokens.
+        Add to the environment:
+
+            export GITHUB_TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+      MESSAGE
+    end
   end
 end
